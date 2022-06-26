34098156ba6127a6e299c5309ba30d65bba701f6
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-27 00:53:57 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,64 +1,79 @@
-const { Children } = require("react");
-function contain(a, b) {
-	let ans = {};
-	let child = {};
-	let flag = 0;
-	let t1 = a.top ? parseInt(a.top) : 0;
-	let b1 = a.bottom ? parseInt(a.bottom) : 0;
-	let l1 = a.left ? parseInt(a.left) : 0;
-	let r1 = a.right ? parseInt(a.right) : 0;
-	let h1 = a.height ? parseInt(a.height) : 0;
-	let w1 = a.width ? parseInt(a.width) : 0;
+function updateStructure(rec1,rec2){
+	//write your code
+	// rec1
+	let rec1Left = rec1.left;
+	rec1Left = Number(rec1Left.substr(0, rec1Left.length-2));
+	let rec1Width = rec1.width;
+	rec1Width = Number(rec1Width.substr(0, rec1Width.length-2));
+	let rec1Top = rec1.top;
+	rec1Top = Number(rec1Top.substr(0, rec1Top.length-2));
+	let rec1Height = rec1.height;
+	rec1Height = Number(rec1Height.substr(0, rec1Height.length-2));
+	// rec2
+	let rec2Left = rec2.left;
+	rec2Left = Number(rec2Left.substr(0, rec2Left.length-2));
+	let rec2Width = rec2.width;
+	rec2Width = Number(rec2Width.substr(0, rec2Width.length-2));
+	let rec2Top = rec2.top;
+	rec2Top = Number(rec2Top.substr(0, rec2Top.length-2));
+	let rec2Height = rec2.height;
+	rec2Height = Number(rec2Height.substr(0, rec2Height.length-2));
+  function checkA() {
+  	if(rec1Top <= rec2Top) {
+  
+      if(rec1Left<= rec2Left) {
 
-	let t2 = b.top ? parseInt(b.top) : 0;
-	let b2 = b.bottom ? parseInt(b.bottom) : 0;
-	let l2 = b.left ? parseInt(b.left) : 0;
-	let r2 = b.right ? parseInt(b.right) : 0;
-	let h2 = b.height ? parseInt(b.height) : 0;
-	let w2 = b.width ? parseInt(b.width) : 0;
+        if(rec1Left+ rec1Width >= rec2Left+rec2Width) {
 
-	if (t2 >= t1 && l2 >= l1 && r2 >= r1 && b2 >= b1) {
-		if ((a.height && (t1 + h1 >= t2 + h2 && b1 + h1 >= b2 + h2) &&
-			(l1 + w1 >= l2 + w2 && r1 + w1 >= r2 + w2)) ||
-			(!a.height)
-		) {
-			if (a.top) {
-				child.top = `${t2 - t1}px`;
-			}
-			if (a.left) {
-				child.left = `${l2 - l1}px`;
-			}
-			if (a.bottom) {
-				child.bottom = `${b2 - b1}px`;
-			}
-			if (a.right) {
-				child.right = `${r2 - r1}px`;
-			}
-			if (a.height) {
-				child.height = `${h2}px`;
-				child.width = `${w2}px`;
-			}
-			ans = { ...a, children: [{ ...child, children: [] }] };
-			flag = 1;
-		}
+          if(rec1Top + rec1Height >= rec2Top + rec2Height) {
+          console.log('here');
+            rec2Top = rec2Top - rec1Top;
+            rec2Left = rec2Left - rec1Left;
+            let ans1 = {...rec1}; 
+            ans1.children = [{
+              top: rec2Left+"px",
+              left: rec2Top + "px",
+              width: rec2Width+ "px",
+              height: rec2Height + "px",
+			  			children: []
+            }]
+            return ans1;
+          }
+          return checkB()
+        }
+        return checkB()
+      }
+      return checkB()
+    }
+    return checkB()
+  }
+  function checkB() {
+  	if(rec1Top >= rec2Top) {
+      if(rec1Left>= rec2Left) {
+        if(rec1Left+ rec1Width <= rec2Left+rec2Width) {
+          if(rec1Top + rec1Height <= rec2Top + rec2Height) {
+            rec1Top = rec1Top - rec2Top;
+            rec1Left = rec1Left - rec2Left;
+            let ans2 = {...rec2}; 
 
-	}
-	if (flag === 0) {
-		return -1;
-	}
-	return ans;
-}
-function updateStructure(rec1, rec2) {
-	let res = contain(rec1, rec2);
-	if (res !== -1) {
-		return res;
-	}
-	res = contain(rec2, rec1);
-	if (res !== -1) {
-		return res;
-	}
-	return rec1;
-	
+            ans2.children = [{
+              top: rec1Left+"px",
+              left: rec1Top + "px",
+              width: rec1Width+ "px",
+              height: rec1Height + "px",
+			  children: []
+            }];
+            return ans2;
+          }
+					return {...rec1};
+        }
+        return {...rec1};
+      }
+      return {...rec1};
+    }
+    return {...rec1};
+  }
+ return checkA()
 }
 
 module.exports = updateStructure;
