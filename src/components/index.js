785db33ebd1b602cc43daddbff1ebf363f7b0bbe
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-06-27 00:44:10 +0530

    Update and rename App.js to index.js

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+//rec = {
+//	top: '25px',
+//	left: '96px',
+//	width: '64px',
+//	height: '96px',
+//      children: []
+//}
+
+function updateStructure(rec1,rec2){
+	//write your code
+	rec1 = {
+		top:'25px',left:'96px',width:'64px',height:'96px',
+		children:[top='10px',
+		left='10px',
+		height='20px',
+		width='30px',]
+	}
+	rec2= {
+		top:'25px',left:'96px',width:'64px',height:'96px',
+		children:[top='10px',
+		left='10px',
+		height='20px',
+		width='30px',]
+	}
+}
+
+module.exports = updateStructure;
